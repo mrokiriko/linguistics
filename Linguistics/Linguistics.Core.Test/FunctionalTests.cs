f6b3be2218236781770665d422bbeb40f700170b
Vladimir Belov <vladimir.belov@hotmail.com>
2016-06-24 17:56:01 +0600

    Added comparison with morpher.ru

--- Linguistics/Linguistics.Core.Test/FunctionalTests.cs
+++ Linguistics/Linguistics.Core.Test/FunctionalTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -38,7 +39,6 @@ namespace Linguistics.Core.Test
 			CheckNames(resourceName);
 		}
 
-
 		[TestMethod]
 		public void CheckAllGirlNamesTest()
 		{
@@ -46,22 +46,56 @@ namespace Linguistics.Core.Test
 			CheckNames(resourceName);
 		}
 
+		private static IDictionary<Case, string> CompareNameCases(IDictionary<Case, string> expected,
+			IDictionary<Case, string> actual)
+		{
+			var result = new Dictionary<Case, string>();
+
+			foreach (var actualNameCase in actual)
+			{
+				var expectedNameCase = expected[actualNameCase.Key];
+
+				if (actualNameCase.Value != expectedNameCase)
+				{
+					result.Add
+						(
+							actualNameCase.Key,
+							string.Format("{0} != {1}", expectedNameCase, actualNameCase.Value)
+						);
+				}
+			}
+			return result;
+		}
+
 		private static void CheckNames(string resourceName)
 		{
 			// Arrange
 			var service = new DeclensionService();
+			var morpher = new MorpherService();
 
 			// Act
 			var errorCount = 0;
 			var okCount = 0;
 			var errorNames = new Collection<string>();
+			var errorNameCases = new Dictionary<string, IDictionary<Case, string>>();
 
-			foreach (var name in GetResourceLines(resourceName))
+			foreach (var name in GetResourceLines(resourceName).Take(20))
 			{
 				try
 				{
+					var parsedName = morpher.DeclineFirstName(name);
 					var nameCases = service.DeclineFirstName(name);
-					okCount++;
+
+					var comparison = CompareNameCases(parsedName, nameCases);
+					if (comparison.Count > 0)
+					{
+						errorNameCases.Add(name, comparison);
+						errorCount++;
+					}
+					else
+					{
+						okCount++;
+					}
 				}
 				catch (Exception)
 				{
@@ -71,8 +105,30 @@ namespace Linguistics.Core.Test
 			}
 
 			// Assert
+			if (errorNameCases.Count > 0)
+			{
+				foreach (var nameCases in errorNameCases)
+				{
+					Console.WriteLine(nameCases.Key);
+					foreach (var nameCase in nameCases.Value)
+					{
+						Console.WriteLine("\t{0} - {1}", nameCase.Key, nameCase.Value);
+					}
+				}
+			}
+
 			Assert.AreEqual(0, errorCount,
 				string.Format("OK: {0}\r\nErrors: {1}\r\nError Names:\r\n{2}", okCount, errorCount, string.Join("\r\n", errorNames)));
+		}
+
+		[TestMethod]
+		public void MorpherServiceTest()
+		{
+			var nameCases = new MorpherService().DeclineFirstName("Иван");
+			foreach (var nameCase in nameCases)
+			{
+				Console.WriteLine("\t{0} - {1}", nameCase.Key, nameCase.Value);
+			}
 		}
 	}
 }
---
+++ Linguistics/Linguistics.Core.Test/MorpherService.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace Linguistics.Core.Test
+{
+	public sealed class MorpherService : IDeclensionService
+	{
+		public IDictionary<Case, string> DeclineFirstName(string firstName)
+		{
+			string responseText;
+			using (var webClient = new WebClient {Encoding = Encoding.UTF8})
+			{
+				webClient.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0");
+				webClient.Headers.Add(HttpRequestHeader.AcceptCharset, "UTF-8");
+				webClient.Proxy.Credentials = CredentialCache.DefaultCredentials;
+
+				var url = "http://api.morpher.ru/WebService.asmx/GetXml?s=" + firstName;
+				responseText = webClient.DownloadString(url);
+			}
+
+			//Console.WriteLine(responseText);
+
+			return ParseReponse(responseText);
+		}
+
+		private IDictionary<Case, string> ParseReponse(string responseText)
+		{
+			var document = new XmlDocument();
+			document.LoadXml(responseText);
+
+			IDictionary<Case, string> result = new Dictionary<Case, string>();
+
+			foreach (XmlNode xmlNode in document.DocumentElement.ChildNodes)
+			{
+				switch (xmlNode.Name)
+				{
+					case @"Р":
+						result.Add(Case.Genitive, xmlNode.InnerText);
+						break;
+
+					case @"Д":
+						result.Add(Case.Dative, xmlNode.InnerText);
+						break;
+
+					case @"В":
+						result.Add(Case.Accusative, xmlNode.InnerText);
+						break;
+
+					case @"Т":
+						result.Add(Case.Instrumental, xmlNode.InnerText);
+						break;
+
+					case @"П":
+						result.Add(Case.Prepositional, xmlNode.InnerText);
+						break;
+
+					case @"ФИО":
+						result.Add(Case.Nominative, xmlNode.ChildNodes[1].InnerText);
+						break;
+				}
+			}
+
+			return result;
+
+			//	{Case.Genitive, document.DocumentElement.SelectSingleNode(@"//s:xml").Value} /*,
+			//{
+			//return new Dictionary<Case, string>
+			//	{Case.Dative, document.SelectSingleNode(@"s://Д").Value},
+			//	{Case.Accusative, document.SelectSingleNode(@"s://В").Value},
+			//	{Case.Instrumental, document.SelectSingleNode(@"s://Т").Value},
+			//	{Case.Prepositional, document.SelectSingleNode(@"s://П").Value},
+			//	{Case.Nominative, document.SelectSingleNode(@"s://ФИО/И").Value}*/
+			//};
+		}
+	}
+}
